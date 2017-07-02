1053265c091bb6758356dcc2995de8c4e34ad3ab
ThieuNv <nguyenthieu2102@gmail.com>
2017-07-03 03:48:13 +0700

    First inital project follow: https://scotch.io/tutorials/mean-app-with-angular-2-and-the-angular-cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
+import {PostsService} from './posts.service';
+
+// Define the routes
+const ROUTES = [
+  {
+    path: '', redirectTo: 'posts', pathMatch: 'full'
+  },
+  {
+    path: 'posts', component: PostsComponent
+  }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES) // Add routes to the app
   ],
-  providers: [],
+  providers: [PostsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
